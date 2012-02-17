995b10772169ccfe4e53bb02445c8c1e9cd7fb8e
Sven A. Schmidt <sas@abstracture.de>
2012-02-17 09:27:56 +0100

    Added CouchUITableSource test

---
+++ Test/Test_CouchUITableSource.m
@@ -0,0 +1,24 @@
+//
+//  Test_CouchUITableSource.m
+//  CouchCocoa
+//
+//  Created by Sven A. Schmidt on 17.02.12.
+//  Copyright (c) 2012 Couchbase, Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface Test_CouchUITableSource : SenTestCase
+@end
+
+
+@implementation Test_CouchUITableSource
+
+// All code under test must be linked into the Unit Test bundle
+- (void)testMath
+{
+    STAssertTrue((1 + 1) == 2, @"Compiler isn't feeling well today :-(");
+}
+
+@end
