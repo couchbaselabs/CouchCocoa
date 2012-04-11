couchbaselabs/CouchCocoa

commit 78c20bd5cdbd900da6515ca511a514acef90446f

if the URL contains basic-auth credentials, use them without waiting for 401 challenge

REST/RESTResource.m
@@ -17,6 +17,7 @@
 
 #import "RESTInternal.h"
 #import "RESTCache.h"
+#import "TDBase64.h"
 
 
 @implementation RESTResource
@@ -162,6 +163,15 @@
     if (queries) {
         NSString* urlStr = [url.absoluteString stringByAppendingString: queries];
         request.URL = [NSURL URLWithString: urlStr];
+    }
+
+    // if the URL contains basic-auth credentials, use them without waiting for 401 challenge
+    NSString* username = [request.URL user];
+    NSString* password = [request.URL password];
+    if (username && password) {
+        NSString *creds = [NSString stringWithFormat:@"%@:%@", username, password];
+        creds = [TDBase64 encode: [creds dataUsingEncoding:NSUTF8StringEncoding]];
+        [request addValue:[NSString stringWithFormat:@"Basic %@", creds] forHTTPHeaderField:@"Authorization"];
     }
 
     return request;

commit 863d6764c2110254b370fdda1efef5d1019fe893

Fix build break (missing TDBase64.h)
Build was broken by previous commit 78c20bd5cdbd.
Replace use of TDBase64 (which exists in Syncpoint but not the other branches) with RESTBase64.

Change-Id: Ic155d8e9761e244a8cdcc0a00982d1ad98ec8caa

REST/RESTResource.m
@@ -17,7 +17,7 @@
 
 #import "RESTInternal.h"
 #import "RESTCache.h"
-#import "TDBase64.h"
+#import "RESTBase64.h"
 
 
 @implementation RESTResource
@@ -170,7 +170,7 @@
     NSString* password = [request.URL password];
     if (username && password) {
         NSString *creds = [NSString stringWithFormat:@"%@:%@", username, password];
-        creds = [TDBase64 encode: [creds dataUsingEncoding:NSUTF8StringEncoding]];
+        creds = [RESTBase64 encode: [creds dataUsingEncoding:NSUTF8StringEncoding]];
         [request addValue:[NSString stringWithFormat:@"Basic %@", creds] forHTTPHeaderField:@"Authorization"];
     }
 