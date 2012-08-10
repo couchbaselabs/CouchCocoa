2e59d45b2ae688dc281dd3b0d2df643648df169a
Jens Alfke <jens@couchbase.com>
2012-08-10 10:07:06 -0700

    Fix deadlock introduced by 2e23298e2c22
    Don't make CouchCocoa calls on the TouchDB thread!

--- Couch/CouchDesignDocument_Embedded.m
+++ Couch/CouchDesignDocument_Embedded.m
@@ -68,16 +68,17 @@ enum {
              reduceBlock: (TDReduceBlock)reduceBlock
                  version: (NSString*)version
 {
+    NSAssert(mapBlock || !reduceBlock, @"Can't set a reduce block without a map block");
     viewName = [self qualifiedName: viewName];
     mapBlock = [mapBlock copy];
     reduceBlock = [reduceBlock copy];
+    TDContentOptions mapContentOptions = self.includeLocalSequence ? kTDIncludeLocalSeq : 0;
     [self tellTDDatabase: ^(TDDatabase* tddb) {
         if (mapBlock) {
             TDView* view = [tddb viewNamed: viewName];
             [view setMapBlock: mapBlock reduceBlock: reduceBlock version: version];
-            view.mapContentOptions = self.includeLocalSequence ? kTDIncludeLocalSeq : 0;
+            view.mapContentOptions = mapContentOptions;
         } else {
-            NSAssert(!reduceBlock, @"Can't set a reduce block without a map block");
             [[tddb existingViewNamed: viewName] deleteView];
         }
     }];

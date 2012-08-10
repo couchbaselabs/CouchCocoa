couchbaselabs/CouchCocoa

commit 2e23298e2c22d965f228f01592c1dd74f0a04c8f

Make design doc .includeLocalSequence option work with TouchDB
Requires the corresponding TouchDB commit that I just pushed.

Couch/CouchDesignDocument_Embedded.m
@@ -12,6 +12,11 @@
 
 
 // Redeclare API from TouchDB to avoid having to #include external headers:
+typedef unsigned TDContentOptions;
+enum {
+    kTDIncludeUpdateSeq = 256                // adds '_update_seq' property
+};
+
 @class TDDatabase, TDRevision, TDView;
 
 @interface TDServer : NSObject
@@ -31,6 +36,7 @@
          reduceBlock: (TDReduceBlock)reduceBlock
              version: (NSString*)version;
 - (void) deleteView;
+@property TDContentOptions mapContentOptions;
 @end
 
 
@@ -69,6 +75,7 @@
         if (mapBlock) {
             TDView* view = [tddb viewNamed: viewName];
             [view setMapBlock: mapBlock reduceBlock: reduceBlock version: version];
+            view.mapContentOptions = self.includeLocalSequence ? kTDIncludeUpdateSeq : 0;
         } else {
             NSAssert(!reduceBlock, @"Can't set a reduce block without a map block");
             [[tddb existingViewNamed: viewName] deleteView];

commit 2e59d45b2ae688dc281dd3b0d2df643648df169a

Fix deadlock introduced by 2e23298e2c22
Don't make CouchCocoa calls on the TouchDB thread!

Couch/CouchDesignDocument_Embedded.m
@@ -68,16 +68,17 @@ enum {
              reduceBlock: (TDReduceBlock)reduceBlock
                  version: (NSString*)version
 {
+    NSAssert(mapBlock || !reduceBlock, @"Can't set a reduce block without a map block");
     viewName = [self qualifiedName: viewName];
     mapBlock = [mapBlock copy];
     reduceBlock = [reduceBlock copy];
+    TDContentOptions mapContentOptions = self.includeLocalSequence ? kTDIncludeLocalSeq : 0;
     [self tellTDDatabase: ^(TDDatabase* tddb) {
         if (mapBlock) {
             TDView* view = [tddb viewNamed: viewName];
             [view setMapBlock: mapBlock reduceBlock: reduceBlock version: version];
-            view.mapContentOptions = self.includeLocalSequence ? kTDIncludeLocalSeq : 0;
+            view.mapContentOptions = mapContentOptions;
         } else {
-            NSAssert(!reduceBlock, @"Can't set a reduce block without a map block");
             [[tddb existingViewNamed: viewName] deleteView];
         }
     }];